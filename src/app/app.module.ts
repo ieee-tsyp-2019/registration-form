b9b5b4883924f8ca2cff09273dd9229cd1eb4edf
TildeHacker <contact@tildehacker.com>
2019-09-26 21:28:32 +0100

    Use LoginComponent at application root
    Signed-off-by: TildeHacker <contact@tildehacker.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,19 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {RouterModule} from '@angular/router';
+import {LoginComponent} from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {path: '', component: LoginComponent}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
