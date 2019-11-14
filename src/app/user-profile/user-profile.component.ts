a3cd7c4679e68b0f169885fe900047a8e6ba2e22
TildeHacker <contact@tildehacker.com>
2019-11-15 00:09:53 +0100

    Add Accommodation interface
    Signed-off-by: TildeHacker <contact@tildehacker.com>

--- src/app/user-profile/user-profile.component.ts
+++ src/app/user-profile/user-profile.component.ts
@@ -6,6 +6,10 @@ import {UserProfile} from '../user-profile';
 import {AngularFireStorage} from '@angular/fire/storage';
 import {AngularFireFunctions} from '@angular/fire/functions';
 
+export interface Accommodation {
+  count: number;
+}
+
 @Component({
   selector: 'app-user-profile',
   templateUrl: './user-profile.component.html',
