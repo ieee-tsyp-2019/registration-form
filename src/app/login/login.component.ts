1c00bd9d75a9dfb10159f36332418ca2b51f581b
TildeHacker <contact@tildehacker.com>
2019-11-15 02:25:57 +0100

    Replace popup by redirect for signing in
    Signed-off-by: TildeHacker <contact@tildehacker.com>

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -25,7 +25,7 @@ export class LoginComponent implements OnInit {
     provider.setCustomParameters({
       hd: 'ieee.org'
     });
-    this.afAuth.auth.signInWithPopup(provider);
+    this.afAuth.auth.signInWithRedirect(provider);
   }
 
   logout() {
