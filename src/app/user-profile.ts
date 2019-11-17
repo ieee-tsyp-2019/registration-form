f5f4efc114c144efff8b0b5b2fc0635f7a12b239
TildeHacker <contact@tildehacker.com>
2019-11-17 18:13:21 +0100

    Remove accommodation attribute from UserProfile model
    Signed-off-by: TildeHacker <contact@tildehacker.com>

--- src/app/user-profile.ts
+++ src/app/user-profile.ts
@@ -8,7 +8,6 @@ export class UserProfile {
     public phoneNumber: string,
     public dateOfBirth: string,
     public studentBranch: string,
-    public accommodation: string,
     public organization: string,
     public position: string,
     public educationalInstitution: string
